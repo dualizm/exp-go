21ee2705cf2f1b48bc243a047d815b1a823a0b38
dualizm <laiker35@yandex.ru>
2024-10-20 01:55:36 +0400

    [upd] end chapter 3

---
+++ ProGo/part-03/tools/main.go
@@ -0,0 +1,21 @@
+// main
+package main
+
+import "fmt"
+
+func main() {
+	PrintHello()
+	for i := 0; i < 5; i++ { // loop with a counter
+		PrintHello()   // print out a message
+		PrintNumber(i) // print out the counter
+	}
+}
+
+func PrintHello() {
+	fmt.Println("Hello, Go")
+}
+
+// PrintNumber writes a number using the fmt.Println function
+func PrintNumber(number int) {
+	fmt.Println(number)
+}
