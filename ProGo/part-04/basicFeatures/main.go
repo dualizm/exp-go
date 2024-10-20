8e165d07537f4b48c21de9b93d80a0c089bfde60
dualizm <laiker35@yandex.ru>
2024-10-21 01:39:21 +0400

    [upd] add part4

---
+++ ProGo/part-04/basicFeatures/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	names := [3]string{"Alice", "Charlie", "Bob"}
+	secondPosition := &names[1]
+
+	fmt.Println(*secondPosition)
+	sort.Strings(names[:])
+	fmt.Println(*secondPosition)
+}
