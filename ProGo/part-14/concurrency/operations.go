edde6c7d5a43f8c2818c44cb4a32b47c1a372ff9
dualizm <laiker35@yandex.ru>
2024-11-15 10:27:56 +0400

    [upd] start part 14

--- ProGo/part-14/concurrency/operations.go
+++ ProGo/part-14/concurrency/operations.go
@@ -4,17 +4,29 @@ import "fmt"
 
 func CalcStoreTotal(data ProductData) {
   var storeTotal float64
+  var channel chan float64 = make(chan float64, 2)
   for category, group := range data {
-    storeTotal += group.TotalPrice(category)
+    go group.TotalPrice(category, channel)
+  }
+
+  fmt.Println("-- Starting to receive from channel")
+  for i := 0; i < len(data); i++ {
+    fmt.Println("-- channel read pending")
+    value := <- channel
+    fmt.Println("-- channel read complete", value)
+    storeTotal += value
   }
   fmt.Println("Total:", ToCurrency(storeTotal))
 }
 
-func (group ProductGroup) TotalPrice(category string) (total float64) {
+func (group ProductGroup) TotalPrice(category string, resultChannel chan float64) {
+  var total float64
   for _, p := range group {
+    //fmt.Println(category, "product:", p.Name)
     total += p.Price
   }
 
-  fmt.Println(category, "subtotal:", ToCurrency(total))
-  return
+  fmt.Println(category, "channel sending", ToCurrency(total))
+  resultChannel <- total
+  fmt.Println(category, "channel send complete")
 }
